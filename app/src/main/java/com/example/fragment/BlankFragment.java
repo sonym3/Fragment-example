77719c5ed21069f638dff7a931c983f6fe8026fb
sonym3 <Bony#9400>
2019-10-06 14:07:48 -0400

    framgent

--- app/src/main/java/com/example/fragment/BlankFragment.java
+++ app/src/main/java/com/example/fragment/BlankFragment.java
@@ -3,19 +3,24 @@ package com.example.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BlankFragment extends Fragment {
+public class BlankFragment extends Fragment implements View.OnClickListener {
 
-
+    Button btn;
+    EditText edt;
+    communicator com;
     public BlankFragment() {
         // Required empty public constructor
     }
@@ -28,4 +33,13 @@ public class BlankFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_blank, container, false);
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
--- app/src/main/java/com/example/fragment/BlankFragment2.java
+++ app/src/main/java/com/example/fragment/BlankFragment2.java
@@ -3,18 +3,23 @@ package com.example.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BlankFragment2 extends Fragment {
+public class BlankFragment2 extends Fragment implements View.OnClickListener{
 
+    Button btn1;
+    EditText edt1;
 
     public BlankFragment2() {
         // Required empty public constructor
@@ -28,4 +33,13 @@ public class BlankFragment2 extends Fragment {
         return inflater.inflate(R.layout.fragment_blank_fragment2, container, false);
     }
 
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
 }
