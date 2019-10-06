14dd6454d44569b8614af9db7a64699bd1230b14
sonym3 <Bony#9400>
2019-10-06 14:39:33 -0400

    framgent

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements communicator {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void respond1(String data) {
+        FragmentManager fm = getSupportFragmentManager();
+        BlankFragment2 f2 = (BlankFragment2) fm.findFragmentById(R.id.fragment2);
+        f2.respond2(data);
+    }
+
+    @Override
+    public void respond2(String data) {
+        FragmentManager fm = getSupportFragmentManager();
+        BlankFragment f1 = (BlankFragment) fm.findFragmentById(R.id.fragment);
+        f1.respond2(data);
     }
 }
